3032e54c88e4d40fa2469841b550646fa23514ef
yoshi suenaga <yoshi@yoshinoMacBook-Air.local>
2024-07-11 21:02:27 +0900

    Initial commit

---
+++ .storybook/main.ts
@@ -0,0 +1,24 @@
+import type { StorybookConfig } from "@storybook/nextjs";
+
+const config: StorybookConfig = {
+  stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  addons: [
+    "@storybook/addon-onboarding",
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@chromatic-com/storybook",
+    "@storybook/addon-interactions",
+  ],
+  framework: {
+    name: "@storybook/nextjs",
+    options: {},
+  },
+  staticDirs: ["../public"],
+  webpackFinal: async (config) => {
+    config.resolve = config.resolve || {};
+    config.resolve.extensions = config.resolve.extensions || [];
+    config.resolve.extensions.push('.ts', '.tsx');
+    return config;
+  },
+};
+export default config;
